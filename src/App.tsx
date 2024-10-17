14cd23376cddb4352f436cf9ea1ee940c5c5bf8a
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-10-17 20:50:45 -0230

    login container

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,16 @@
-function MyHeading() {
-  return (
-      <div className="nav_container">
-        <nav className="nav_heading">
-          <h1>This is a heading</h1>
-          <div className="nav_items">
-            <ul>item1</ul>
-            <ul>item2</ul>
-            <ul>item3</ul>
-            <ul>item4</ul>
-          </div>
-        </nav>
-      </div>
-  );
-}
-function button() {
-  
-}
+import LoginContainer from './Login.tsx'
 
 function MyBody() {
   return (
     <main>
-      <div className="body_class">
-        <button className="button_class">This is a button</button>
-        <p>this ia a sentence</p>
-        <h2>{user.name}</h2>
-        <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-      </div>
+      <LoginContainer />
     </main>
   );
 }
-const user = {
-  name: 'ammar',
-  imageUrl: 'https://avatars.githubusercontent.com/u/71677758?v=4',
-  imageSize: 90,
-};
 
 export default function MyApp() {
   return (
     <>
-      <MyHeading />
       <MyBody />
     </>
   );
---
+++ src/Login.tsx
@@ -0,0 +1,18 @@
+function LoginContainer() {
+    return (
+        <div className="loginContainer">
+            <div className="loginHeader">
+                <h2>Housekeepr</h2>
+            </div>
+            <div className="inputContainer">
+                <div className="userName">
+                    <input>Username</input>
+                </div>
+                <div className="Password">
+                    <input>Password</input>
+                </div>
+            </div>
+        </div>
+    );
+  }
+export default LoginContainer;
