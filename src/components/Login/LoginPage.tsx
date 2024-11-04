341e9db1489048fcfbb4422e7cc70d23f2a03ded
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-11-04 13:24:01 -0330

    i love tailwind, login page design draftes

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./LoginPage.css";
 import user_icon from "../assets/user_icon.svg";
 import password_icon from "../assets/password_icon.svg";
 
@@ -12,7 +11,7 @@ const LoginPage = () => {
   const handleLogin = () => {
     if (username === "admin" && password === "admin") {
       alert("Login successful!");
-      navigate("/dashboard"); 
+      navigate("/dashboard");
     } else {
       alert("Invalid credentials. Please try again.");
     }
@@ -25,24 +24,43 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="loginpagecontainer">
-      <div className="header">
-        <div className="text">Log in</div>
+    <div
+      className="
+    loginpagecontainer
+    mt-40%
+    max-lg:mt-60% 
+    max-sm:mt-45% 
+    p-2r
+    max-lg:p-1r
+    flex 
+    flex-col
+    justify-center 
+    content-center
+    gap-3 
+    rounded-lg
+    drop-shadow-lg
+    bg-white
+    "
+    >
+      <div className="header mb-4">
+        <div className="text text-3xl text-wine">Housekeepr</div>
       </div>
-      <div className="inputs">
-        <div className="input">
+      <div className="inputs flex flex-col">
+        <div className="input flex m-2 outline outline-1 outline-lightgrey p-1 rounded-lg">
           <img src={user_icon} alt="User Icon" />
           <input
+            className="bg-transparent rounded-md bg-pearl pl-1"
             type="text"
             placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
-        <div className="input">
+        <div className="input flex m-2 outline outline-1 outline-lightgrey p-1 rounded-lg">
           <img src={password_icon} alt="Password Icon" />
           <input
-            type="password"
+            className="bg-transparent rounded-md bg-pearl pl-1"
+            type="text"
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -50,7 +68,7 @@ const LoginPage = () => {
           />
         </div>
       </div>
-      <div className="submit-container">
+      <div className="submit-container flex self-center justify-center bg-wine text-white hover:bg-dustyblue rounded-lg p-1 w-20">
         <div className="submit">
           <button className="login-button" onClick={handleLogin}>
             Log In
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '1r': '3.25rem',
+        '2r': '5.75rem',
+        '3r': '7rem',
+        '4r': '10rem',
+        '5r': '15rem',
+        '10%': '10%',
+        '15%': '15%',
+        '20%': '20%',
+        '25%': '25%',
+        '30%': '30%',
+        '35%': '35%',
+        '40%': '40%',
+        '45%': '45%',
+        '50%': '50%',
+        '55%': '55%',
+        '60%': '60%',
+        '65%': '65%',
+      }
+    },
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      pearl: "#f2f2eb",
+      mistysky: "#d2dce6",
+      dustyblue: "#9cabb4",
+      clay: "#ab644b",
+      wine: "#72383d",
+      chocolate: "#401b1b",
+      lightgrey: "#aaaaaa",
+    },
   },
   plugins: [],
-}
-
+};
