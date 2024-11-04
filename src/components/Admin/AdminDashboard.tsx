51fcdc2632d8bb7b1ca615ba77e13bb4eb5fef77
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-11-04 17:14:12 -0330

    i cant justify between a conatainer, i am cooked

--- src/components/Admin/AdminDashboard.tsx
+++ src/components/Admin/AdminDashboard.tsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
-import AdminDashboardHsk from './AdminDashboardHsk';
-import AdminDashboardSup from './AdminDashboardSup';
-import './AdminDashboard.css';
+import React, { useState } from "react";
+import AdminDashboardHsk from "./AdminDashboardHsk";
+import AdminDashboardSup from "./AdminDashboardSup";
+import "./AdminDashboard.css";
 
 const AdminDashboard = () => {
   return (
-    <div className="dashboard-container m-0 py-20">
-      <nav className="navbar fixed px-10 py-3 top-0 left-0 right-0 flex justify-between items-center bg-chocolate">
-        <div className="navbar-left">
-        </div>
+    <div className="dashboard-container flex flex-col m-0 py-20">
+      <nav className="navbar fixed px-10 py-3 top-0 left-0 right-0 flex justify-end items-center bg-chocolate text-white">
         <div className="navbar-right">
-          <ul>
+          <ul className="flex flex-column gap-5 m-0">
             <li>Dashboard</li>
             <li>Housekeeper</li>
             <li>Supervisors</li>
@@ -18,16 +16,16 @@ const AdminDashboard = () => {
           </ul>
         </div>
       </nav>
-      <div className="dashboard-header w-full flex items-center justify-between">
-        <h1>Dashboard</h1>
-        <div className="dashboard-stats">
-          <div className="stats-box">
+      <div className="dashboard-header flex justify-between items-center m-0 mb-10 px-10">
+        <h1 className="text-3xl text-wine">Dashboard</h1>
+        <div className="dashboard-stats flex bg-clay text-white rounded-md px-3 py-1.5">
+          <div className="stats-box px-1">
             <span>Dirty Rooms: 32</span>
           </div>
-          <div className="stats-box">
+          <div className="stats-box px-1">
             <span>Clean Rooms: 20</span>
           </div>
-          <div className="stats-box">
+          <div className="stats-box px-1">
             <span>Inspected Rooms: 15</span>
           </div>
         </div>
