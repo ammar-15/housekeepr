a7c9fb4d65e40628524e1d8a497ca8cf34a336eb
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-12-04 13:38:34 -0330

    hsk room conatiner is now connected to RoomData

--- src/components/Admin/HSKRoomContainer.tsx
+++ src/components/Admin/HSKRoomContainer.tsx
@@ -1,32 +1,26 @@
-import RoomData from '../RoomData.tsx'
-
+import RoomData from "../RoomData.tsx";
 
 const HSKRoomContainer = () => {
-    const roomData = {
-      roomNumber: "301",
-      roomtype: "NK",
-      roomstatus: "Dirty",
-      co_status: "DUE",
-      people: "2/0",
-      arrival: "12/Oct/2024",
-      departs: "13/Oct/2024",
-      extras: "Rollaway Bed",
-      early_ci: "2 PM",
-    };
-  
-    return (
-      <div className="flex items-center justify-between p-4 bg-white rounded-md shadow-md mb-2">
-        <p className="flex-1 text-center">{roomData.roomNumber}</p>
-        <p className="flex-1 text-center">{roomData.roomtype}</p>
-        <p className="flex-1 text-center">{roomData.co_status}</p>
-        <p className="flex-1 text-center">{roomData.people}</p>
-        <p className="flex-1 text-center">{roomData.arrival}</p>
-        <p className="flex-1 text-center">{roomData.departs}</p>
-        <p className="flex-1 text-center">{roomData.extras}</p>
-        <p className="flex-1 text-center">{roomData.early_ci}</p>
-      </div>
-    );
-  };
-  
-  export default HSKRoomContainer;
-  
+
+  const room = RoomData.find((room) => room.roomNumber === "104");
+  if (!room) {
+    return <div>Room not found</div>;
+  }
+  return (
+        <div
+          className="HSKRoomContainer flex items-center justify-between p-4 bg-white rounded-md shadow-md mb-2"
+        >
+          <p className="flex-1 text-center">{room.roomNumber}</p>
+          <p className="flex-1 text-center">{room.roomType}</p>
+          <p className="flex-1 text-center">{room.roomStatus}</p>
+          <p className="flex-1 text-center">{room.coStatus}</p>
+          <p className="flex-1 text-center">{room.people}</p>
+          <p className="flex-1 text-center">{room.arrival}</p>
+          <p className="flex-1 text-center">{room.departs}</p>
+          <p className="flex-1 text-center">{room.extras}</p>
+          <p className="flex-1 text-center">{room.early_ci}</p>
+        </div>
+  );
+};
+
+export default HSKRoomContainer;
