a96ebc14fa5fd33965a2081e9d633ccbba30f8f0
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-12-16 14:31:25 -0330

    one firestore collection for everytihing

--- src/components/Admin/AdminSUP.tsx
+++ src/components/Admin/AdminSUP.tsx
@@ -1,28 +1,23 @@
 import { useState, useEffect } from "react";
-import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import { db } from "../../../firebase";
 import HSKRoomContainer from "./HSKRoomContainer";
 import AdminNavbar from "./AdminNavbar";
 import AdminStart from "./Admin Button/AdminStart";
 
 const AdminSUP = () => {
-  const [SUProomNumbers, setSUProomNumbers] = useState<string[]>([]);
+  const [SUProoms, setSUProoms] = useState<any[]>([]);
 
   useEffect(() => {
-    const fetchRooms = async () => {
-      const roomsCollectionRef = collection(db, "AdminSUP");
-      const snapshot = await getDocs(roomsCollectionRef);
-      const fetchedRooms = snapshot.docs.map((doc) => doc.data().roomNumber);
-      setSUProomNumbers(fetchedRooms);
-    };
-    fetchRooms();
-    const roomsCollectionRef = collection(db, "AdminSUP");
+    const roomsCollectionRef = collection(db, "AdminHSK");
     const unsubscribe = onSnapshot(roomsCollectionRef, (snapshot) => {
-      const updatedRooms = snapshot.docs.map((doc) => doc.data().roomNumber);
-      setSUProomNumbers(updatedRooms);
+      const allRooms = snapshot.docs.map((doc) => doc.data());
+      const cleanRooms = allRooms.filter((room) => room.roomStatus === "Clean"); 
+      setSUProoms(cleanRooms);
     });
     return () => unsubscribe();
   }, []);
+
   return (
     <div className="dashboard-container flex flex-col m-0 py-20 px-10">
       <AdminNavbar />
@@ -30,19 +25,15 @@ const AdminSUP = () => {
         <h1 className="text-3xl text-wine">Supervisors</h1>
         <div className="dashboard-stats flex bg-clay text-white rounded-md px-3 py-1.5">
           <div className="stats-box px-2">Total Supervisors: 12</div>
-          <div className="stats-box px-2">Total Rooms to Inspect: 50</div>
+          <div className="stats-box px-2">Total Rooms to Inspect: {SUProoms.length}</div>
         </div>
       </div>
       <div className="section-container">
-        {SUProomNumbers.map((roomNumber, index) => (
-          <HSKRoomContainer key={index} roomNumber={roomNumber} />
+        {SUProoms.map((room, index) => (
+          <HSKRoomContainer key={index} room={room} />
         ))}
       </div>
-      <AdminStart
-        onAddSUProom={(newRoom) => {
-          setSUProomNumbers((prev) => [...prev, newRoom]);
-        }}
-      />
+      <AdminStart />
     </div>
   );
 };
