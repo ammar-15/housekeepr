7bb2e4c1ff5deb35950190e18f5110333c14cdf7
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-02-17 04:57:23 -0330

    added HSK assign for room assign

--- src/components/Admin/Admin Button/AdminHSKassign.tsx
+++ src/components/Admin/Admin Button/AdminHSKassign.tsx
@@ -5,11 +5,13 @@ import RoomData from "../../RoomData";
 
 interface AdminHSKassignProps {
   onAddHSKroom?: (HSKroomNumber: string) => void;
+  onAssignedTo?: (AssignedTo: string) => void;
   onClose?: () => void;
 }
 
-const AdminHSKassign = ({ onAddHSKroom, onClose }: AdminHSKassignProps) => {
+const AdminHSKassign = ({ onAddHSKroom, onAssignedTo, onClose }: AdminHSKassignProps) => {
   const [HSKroomNumber, setHSKRoomNumber] = useState(""); 
+  const [AssignedTo, setAssignedTo] = useState("");
   const [statusOption, setStatusOption] = useState("Dirty"); 
   const handleClose = () => {
     if (onClose) {
@@ -34,18 +36,21 @@ const AdminHSKassign = ({ onAddHSKroom, onClose }: AdminHSKassignProps) => {
 
       try {
         const roomRef = doc(db, "AdminHSK", HSKroomNumber);
-        const updatedRoom = { ...room, roomStatus: statusOption, time_stamp: formattedTime };
+        const updatedRoom = { ...room, roomStatus: statusOption, assignedto: AssignedTo, time_stamp: formattedTime };
         await setDoc(roomRef, updatedRoom);
         console.log(`room saved to Firebase with status "${statusOption}" at "${formattedTime}":`, updatedRoom);
 
         if (onAddHSKroom) onAddHSKroom(HSKroomNumber);
+        if (onAssignedTo) onAssignedTo(AssignedTo);
+        console.log(HSKroomNumber, setAssignedTo);
+
         setHSKRoomNumber(""); 
+        setAssignedTo(""); 
       } catch (error) {
-        console.error("firebase error", error);
+        console.error("firebase room error", error);
       }
-    }
-    console.log("HSK assign done!!");
-  };
+    };
+};
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
@@ -56,6 +61,13 @@ const AdminHSKassign = ({ onAddHSKroom, onClose }: AdminHSKassignProps) => {
           placeholder="Room Number"
           value={HSKroomNumber}
           onChange={(e) => setHSKRoomNumber(e.target.value)}
+          className="w-full mb-3 p-2 border rounded-md"
+        />
+        <input
+          type="text"
+          placeholder="Assigned To"
+          value={AssignedTo}
+          onChange={(e) => setAssignedTo(e.target.value)}
           className="w-full mb-3 p-2 border rounded-md"
         />
         <select
