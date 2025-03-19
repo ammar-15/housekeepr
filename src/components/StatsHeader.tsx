69be8dda46b60e72ec0f3479b49ddf2dac542e9a
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2025-03-19 16:19:08 -0230

    stats header elements are now objects

--- src/components/StatsHeader.tsx
+++ src/components/StatsHeader.tsx
@@ -15,63 +15,71 @@ interface StatsHeaderProps {
 }
 
 const StatsHeader = ({ pagename, stats }: StatsHeaderProps) => {
-  return (
-    <div className="dashboard-stats ">
-      {pagename === "AdminDashboard" && (
-        <div className="stats-dashoard flex bg-clay text-white rounded-md px-3 py-1.5">
-          <div className="stats-box px-2">
-            <span>Dirty Rooms: {stats.dirtyRooms}</span>
-          </div>
-          <div className="stats-box px-2">
-            <span>Clean Rooms: {stats.cleanRooms}</span>
-          </div>
-          <div className="stats-box px-2">
-            <span>Inspected Rooms: {stats.inspectedRooms}</span>
-          </div>
-        </div>
-      )}
-
-      {pagename === "AdminHSK" && (
-        <div className="stats-HSK flex bg-clay text-white rounded-md px-3 py-1.5">
-          <div className="stats-box px-2">
-            <span>Total Housekeepers: {stats.totalHousekeepers}</span>
-          </div>
-          <div className="stats-box px-2">
-            <span>Total Rooms to Clean: {stats.totalRoomsToClean}</span>
-          </div>
-        </div>
-      )}
-
-      {pagename === "AdminSUP" && (
-        <div className="stats-HSK flex bg-clay text-white rounded-md px-3 py-1.5">
-          <div className="stats-box px-2">
-            <span>Total Supervisors: {stats.totalSupervisors}</span>
-          </div>
-          <div className="stats-box px-2">
-            <span>Total Rooms to Inspect: {stats.totalRoomsToInspect}</span>
-          </div>
-        </div>
-      )}
-
-      {pagename === "AdminRooms" && (
-        <div className="stats-box flex bg-clay text-white rounded-md px-5 py-1.5">
-          <span>Total Rooms: {stats.totalRooms}</span>
-        </div>
-      )}
-
-      {pagename === "HSKfilter" && (
-        <div className="stats-box flex bg-clay text-white rounded-md px-5 py-1.5">
-          <span>Total Rooms to Clean: {stats.totalRoomsToClean}</span>
-        </div>
-      )}
-
-      {pagename === "SUPfilter" && (
-        <div className="stats-box flex bg-clay text-white rounded-md px-5 py-1.5">
-          <span>Total Rooms to Inspect: {stats.totalRoomsToInspect}</span>
-        </div>
-      )}
+  const adminDashboardStats = (
+    <div className="stats-dashoard flex bg-clay text-white rounded-md px-3 py-1.5">
+      <div className="stats-box px-2">
+        <span>Dirty Rooms: {stats.dirtyRooms}</span>
+      </div>
+      <div className="stats-box px-2">
+        <span>Clean Rooms: {stats.cleanRooms}</span>
+      </div>
+      <div className="stats-box px-2">
+        <span>Inspected Rooms: {stats.inspectedRooms}</span>
+      </div>
     </div>
   );
+
+  const adminHSKStats = (
+    <div className="stats-HSK flex bg-clay text-white rounded-md px-3 py-1.5">
+      <div className="stats-box px-2">
+        <span>Total Housekeepers: {stats.totalHousekeepers}</span>
+      </div>
+      <div className="stats-box px-2">
+        <span>Total Rooms to Clean: {stats.totalRoomsToClean}</span>
+      </div>
+    </div>
+  );
+
+  const adminSUPStats = (
+    <div className="stats-HSK flex bg-clay text-white rounded-md px-3 py-1.5">
+      <div className="stats-box px-2">
+        <span>Total Supervisors: {stats.totalSupervisors}</span>
+      </div>
+      <div className="stats-box px-2">
+        <span>Total Rooms to Inspect: {stats.totalRoomsToInspect}</span>
+      </div>
+    </div>
+  );
+
+  const adminRoomsStats = (
+    <div className="stats-box flex bg-clay text-white rounded-md px-5 py-1.5">
+      <span>Total Rooms: {stats.totalRooms}</span>
+    </div>
+  );
+
+  const hskFilterStats = (
+    <div className="stats-box flex bg-clay text-white rounded-md px-5 py-1.5">
+      <span>Total Rooms to Clean: {stats.totalRoomsToClean}</span>
+    </div>
+  );
+
+  const supFilterStats = (
+    <div className="stats-box flex bg-clay text-white rounded-md px-5 py-1.5">
+      <span>Total Rooms to Inspect: {stats.totalRoomsToInspect}</span>
+    </div>
+  );
+
+return (
+  <div className="dashboard-stats">
+      {pagename === "AdminDashboard" && adminDashboardStats}
+      {pagename === "AdminHSK" && adminHSKStats}
+      {pagename === "AdminSUP" && adminSUPStats}
+      {pagename === "AdminRooms" && adminRoomsStats}
+      {pagename === "HSKfilter" && hskFilterStats}
+      {pagename === "SUPfilter" && supFilterStats}
+    </div>
+)
+
 };
 
 export default StatsHeader;
